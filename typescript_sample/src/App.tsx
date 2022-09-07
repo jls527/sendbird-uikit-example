dfdb0898ffcf368a54d08c5219362f6ee00a1bf2
Jesper Lund <jesper.lund@schibsted.com>
2022-09-07 11:51:15 +0200

    Add example code

--- typescript_sample/src/App.tsx
+++ typescript_sample/src/App.tsx
@@ -1,35 +1,76 @@
-import './App.css';
-import '@sendbird/uikit-react/dist/index.css';
+import "./App.css";
+// import "@sendbird/uikit-react/dist/index.css";
 
-import React from 'react';
+import React from "react";
 
-import SendbirdProvider from '@sendbird/uikit-react/SendbirdProvider';
-import ChannelList from '@sendbird/uikit-react/ChannelList';
-import Channel from '@sendbird/uikit-react/Channel';
+import SendbirdProvider from "@sendbird/uikit-react/SendbirdProvider";
+import ChannelList from "@sendbird/uikit-react/ChannelList";
+import Channel from "@sendbird/uikit-react/Channel";
 
-import { APP_ID, USER_ID, NICKNAME } from './const';
+import { APP_ID, USER_ID, NICKNAME } from "./const";
+import { ClientUserMessage } from "SendbirdUIKitGlobal";
 
 function App() {
-  const [currentChannelUrl, setCurrentChannelUrl] = React.useState('');
+  const [currentChannelUrl, setCurrentChannelUrl] = React.useState("");
   return (
     <div className="App">
       <SendbirdProvider appId={APP_ID} userId={USER_ID} nickname={NICKNAME}>
-        <div className="sendbird-app__channellist-wrap">
-          {/* ChannelList has wrong props will fix in 2.0.1
-          // @ts-ignore */}
-          <ChannelList
-            onChannelSelect={(channel) => {
-              if (channel?.url) {
-                setCurrentChannelUrl(channel.url);
-              }
-            }}
-          />
-        </div>
-        <div className="sendbird-app__conversation-wrap">
-          <Channel
-            channelUrl={currentChannelUrl}
-          />
-        </div>
+        <ChannelList
+          onChannelSelect={(channel) => {
+            if (channel?.url) {
+              setCurrentChannelUrl(channel.url);
+            }
+          }}
+          renderPlaceHolderEmptyList={() => (
+            <div className="box">
+              Render Empty List State (renderPlaceHolderEmptyList)
+            </div>
+          )}
+          renderPlaceHolderLoading={() => (
+            <div className="box">
+              Render Loading State (renderPlaceHolderLoading)
+            </div>
+          )}
+          renderPlaceHolderError={() => (
+            <div className="box">
+              Render Error State (renderPlaceHolderError)
+            </div>
+          )}
+          renderChannelPreview={({ channel }) => (
+            <div className="channel-list-item">
+              {channel.name} {channel.url}
+            </div>
+          )}
+        />
+        <Channel
+          channelUrl={currentChannelUrl}
+          renderChannelHeader={() => (
+            <div className="channel-header">
+              Channel header (renderChannelHeader)
+            </div>
+          )}
+          renderMessage={({ message }) => (
+            <div className="message">
+              <div>{(message as ClientUserMessage)?.message}</div>
+              {new Date(message.createdAt).toLocaleTimeString()}
+            </div>
+          )}
+          renderPlaceholderLoader={() => (
+            <div className="box">
+              Render Loading State (renderPlaceHolderLoader)
+            </div>
+          )}
+          renderPlaceholderEmpty={() => (
+            <div className="box">
+              Render Empty State (renderPlaceholderEmpty)
+            </div>
+          )}
+          renderPlaceholderInvalid={() => (
+            <div className="box">
+              Render Invalid State (renderPlaceholderInvalid)
+            </div>
+          )}
+        />
       </SendbirdProvider>
     </div>
   );
